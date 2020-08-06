03ffc8b50a71abec96d4c40daa567b7576dde96c
Admin <thienvu453@gmail.com>
2020-08-06 13:37:59 +0700

    Project init

---
+++ src/main/java/com/nancy/m6project/M6ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nancy.m6project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M6ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M6ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nancy/m6project/M6ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nancy.m6project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M6ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
