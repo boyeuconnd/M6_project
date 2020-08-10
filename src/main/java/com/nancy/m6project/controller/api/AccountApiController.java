c4c9e5388f684062925f3fc4284a9e6a93a5e14a
Admin <thienvu453@gmail.com>
2020-08-10 12:33:32 +0700

    CrossOrigin resolve

--- src/main/java/com/nancy/m6project/controller/api/AccountApiController.java
+++ src/main/java/com/nancy/m6project/controller/api/AccountApiController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 //@RequestMapping("api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AccountApiController {
     @Autowired
     private JwtUntil jwtUntil;
