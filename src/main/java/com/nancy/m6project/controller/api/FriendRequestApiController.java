d084372816e41da97cd265305f5f4740e4798c62
DungTrinhQ <dungtrinh0702@gmail.com>
2020-08-12 14:56:59 +0700

    viết xong phương thức chấp nhận/xóa friendRQ, get friendlist

--- src/main/java/com/nancy/m6project/controller/api/FriendRequestApiController.java
+++ src/main/java/com/nancy/m6project/controller/api/FriendRequestApiController.java
@@ -1,15 +1,16 @@
 package com.nancy.m6project.controller.api;
 
+import com.nancy.m6project.model.account.Account;
 import com.nancy.m6project.model.friendRequest.FriendRequest;
 import com.nancy.m6project.service.account.AccountService;
 import com.nancy.m6project.service.friendRequest.FriendRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class FriendRequestApiController {
@@ -44,4 +45,54 @@ public class FriendRequestApiController {
         message = "fail";
         return message;
     }
+
+    @PutMapping("/api/friend_request_response/{friend_request_id}")
+    public String AcceptFriendRequest(@PathVariable Long friend_request_id) throws SQLIntegrityConstraintViolationException {
+        String messege = "";
+        FriendRequest friendRequest = friendRequestService.getFriendRequestById(friend_request_id);
+        if (friendRequest != null) {
+            friendRequest.setStatus(ACCEPT);
+            friendRequestService.save(friendRequest);
+            messege = "success";
+        } else {
+            messege = "fail";
+        }
+        return messege;
+    }
+
+    @DeleteMapping("/api/friend_request_response/{friend_request_id}")
+    public String DeleteFriendRequest(@PathVariable Long friend_request_id) {
+        String message = "";
+        FriendRequest friendRequest = friendRequestService.getFriendRequestById(friend_request_id);
+        friendRequestService.delete(friend_request_id);
+        message = "xóa thành công";
+        return message;
+
+    }
+
+    @GetMapping("/api/{account_id}/friends")
+    public List<Account> getAllFriendList(@PathVariable Long account_id) {
+        List<FriendRequest> friendRequestList = friendRequestService.getAllFriendByAccountId(account_id, ACCEPT, account_id, ACCEPT);
+        List<Account> accountList = new ArrayList<>();
+        for (FriendRequest friendRequest : friendRequestList) {
+            if (friendRequest.getAccountSend().getId() != account_id) {
+                accountList.add(friendRequest.getAccountSend());
+            } else {
+                accountList.add(friendRequest.getAccountReceive());
+            }
+        }
+        return accountList;
+    }
+
+    @GetMapping("/api/{account_id}/friend_request_response")
+    public List<Account> getListAccountRequestToMe(@PathVariable Long account_id) {
+        List<Account> accountList = new ArrayList<>();
+        List<FriendRequest> friendRequestList = friendRequestService.getAllFriendRequestAccountReceived(account_id, PENDING);
+        for (FriendRequest friendRequest : friendRequestList) {
+            accountList.add(friendRequest.getAccountSend());
+        }
+        return accountList;
+    }
+
+
 }
--- src/main/java/com/nancy/m6project/service/friendRequest/FriendRequestService.java
+++ src/main/java/com/nancy/m6project/service/friendRequest/FriendRequestService.java
@@ -6,11 +6,19 @@ import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
 public interface FriendRequestService {
+    FriendRequest getFriendRequestById(Long friendRequestId);
+
+    List<FriendRequest> getAllFriendRequestAccountReceived(Long accountId, Integer status);
+
     boolean isFriend(Long accountSendId1, Long accountReceiveId2, Integer status);
 
     FriendRequest save(FriendRequest model) throws SQLIntegrityConstraintViolationException;
 
-    Boolean existsByAccountSendIdOrAccountReceiveId(Long accountSent,Long accountReceive);
+    boolean delete(Long id);
 
-    Boolean existsFriendRequestByAccountReceive_IdAndAccountSend_Id(Long sentId,Long receiveId);
+    Boolean existsByAccountSendIdOrAccountReceiveId(Long accountSent, Long accountReceive);
+
+    Boolean existsFriendRequestByAccountReceive_IdAndAccountSend_Id(Long sentId, Long receiveId);
+
+    List<FriendRequest> getAllFriendByAccountId(Long accountSendId, Integer status1, Long accountReceiveId, Integer status2);
 }
