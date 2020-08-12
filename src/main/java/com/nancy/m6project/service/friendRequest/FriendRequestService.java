f75f115e07a4e4e71fb71f3555e4a25e8b52c6b0
DungTrinhQ <dungtrinh0702@gmail.com>
2020-08-12 10:03:17 +0700

    viết phương thức gửi yêu cầu kết bạn

--- src/main/java/com/nancy/m6project/service/friendRequest/FriendRequestService.java
+++ src/main/java/com/nancy/m6project/service/friendRequest/FriendRequestService.java
@@ -1,2 +1,16 @@
-package com.nancy.m6project.service.friendRequest;public interface FriendRequestService {
+package com.nancy.m6project.service.friendRequest;
+
+import com.nancy.m6project.model.friendRequest.FriendRequest;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
+
+public interface FriendRequestService {
+    boolean isFriend(Long accountSendId1, Long accountReceiveId2, Integer status);
+
+    FriendRequest save(FriendRequest model) throws SQLIntegrityConstraintViolationException;
+
+    Boolean existsByAccountSendIdOrAccountReceiveId(Long accountSent,Long accountReceive);
+
+    Boolean existsFriendRequestByAccountReceive_IdAndAccountSend_Id(Long sentId,Long receiveId);
 }
--- src/main/java/com/nancy/m6project/service/impl/FriendRequestServiceImpl.java
+++ src/main/java/com/nancy/m6project/service/impl/FriendRequestServiceImpl.java
@@ -1,2 +1,43 @@
-package com.nancy.m6project.service.impl;public class FriendRequestServiceImpl {
+package com.nancy.m6project.service.impl;
+
+import com.nancy.m6project.model.friendRequest.FriendRequest;
+import com.nancy.m6project.repositories.friendRequest.FriendRequestRepositories;
+import com.nancy.m6project.service.friendRequest.FriendRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
+
+@Service
+public class FriendRequestServiceImpl implements FriendRequestService {
+
+    @Autowired
+    private FriendRequestRepositories friendRequestRepositories;
+
+    @Override
+    public boolean isFriend(Long accountSendId1, Long accountReceiveId2, Integer status) {
+        FriendRequest friendRequest = friendRequestRepositories.findByAccountSendIdAndAccountReceiveIdAndStatus(accountSendId1, accountReceiveId2, status);
+        FriendRequest friendRequestReverse = friendRequestRepositories.findByAccountSendIdAndAccountReceiveIdAndStatus(accountReceiveId2, accountSendId1, status);
+        if (friendRequest != null || friendRequestReverse != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public FriendRequest save(FriendRequest model) {
+        return friendRequestRepositories.save(model);
+    }
+
+    @Override
+    public Boolean existsByAccountSendIdOrAccountReceiveId(Long accountSent, Long accountReceive) {
+        return friendRequestRepositories.existsByAccountSendIdOrAccountReceiveId(accountSent,accountReceive);
+    }
+
+    @Override
+    public Boolean existsFriendRequestByAccountReceive_IdAndAccountSend_Id(Long sentId, Long receiveId) {
+        return friendRequestRepositories.existsFriendRequestByAccountReceive_IdAndAccountSend_Id(sentId,receiveId);
+    }
 }
