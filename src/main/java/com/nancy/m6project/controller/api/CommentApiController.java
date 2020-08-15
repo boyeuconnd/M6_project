636d98e2279237501abb23c821f6b3af90b0948d
Longsugar88 <mrlongdung@gmail.com>
2020-08-15 15:15:26 +0700

    edit va delete comment

--- src/main/java/com/nancy/m6project/controller/api/CommentApiController.java
+++ src/main/java/com/nancy/m6project/controller/api/CommentApiController.java
@@ -1,15 +1,20 @@
 package com.nancy.m6project.controller.api;
 
+import com.nancy.m6project.model.account.Account;
 import com.nancy.m6project.model.comment.Comment;
 import com.nancy.m6project.model.status.Status;
+import com.nancy.m6project.service.account.AccountService;
 import com.nancy.m6project.service.comment.CommentService;
+import com.nancy.m6project.service.friendRequest.FriendRequestService;
 import com.nancy.m6project.service.status.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestController
 public class CommentApiController {
@@ -17,17 +22,40 @@ public class CommentApiController {
     CommentService commentService;
     @Autowired
     StatusService statusService;
+    @Autowired
+    FriendRequestService friendRequestService;
+
+    @Autowired
+    AccountService accountService;
 
     @PostMapping("api/comment-create/{id}")
-    public ResponseEntity<Comment> createComment(@RequestBody Comment comment, @PathVariable Long id, Principal principal){
+    public ResponseEntity<Comment> createComment(@RequestBody Comment comment, @PathVariable Long id, Principal principal) throws SQLIntegrityConstraintViolationException {
         Status status = statusService.findOne(id);
+        Account account = comment.getAccount();
         comment.setStatus(status);
-        commentService.save(comment);
+        if(friendRequestService.checkRelation(account.getId(), status.getAccount().getId()).equals("friend") || account.getId() == status.getAccount().getId()){
+            commentService.save(comment);
+        }
         return new ResponseEntity<>(comment, HttpStatus.CREATED);
     }
     @GetMapping("api/comment-getAll/{id}")
     public Iterable<Comment> getAllCommentByStatusID(@PathVariable Long id){
         Iterable<Comment> commentList = commentService.findCommentByStatusIdOrderByCreatedDateAsc(id);
         return commentList;
+    }
+    @PutMapping("api/comment-edit/{id_account}/{id_status}")
+    public ResponseEntity<Comment> editComment(@RequestBody Comment comment, @PathVariable("id_account") Long id_acocunt, @PathVariable("id_status") Long id_status) throws SQLIntegrityConstraintViolationException {
+        Account account = accountService.findOne(id_acocunt);
+        Status status = statusService.findOne(id_status);
+        comment.setStatus(status);
+        if(comment.getAccount().getId().equals(account.getId())){
+            commentService.save(comment);
+        }
+        return new ResponseEntity<>(comment, HttpStatus.OK);
+    }
+    @DeleteMapping("api/comment-delete/{id}")
+    public ResponseEntity<Void> deleteComment(@RequestParam Long id){
+        commentService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/nancy/m6project/service/impl/CommentServiceImpl.java
+++ src/main/java/com/nancy/m6project/service/impl/CommentServiceImpl.java
@@ -4,22 +4,62 @@ import com.nancy.m6project.model.comment.Comment;
 import com.nancy.m6project.repositories.comment.CommentRepository;
 import com.nancy.m6project.service.comment.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class CommentServiceImpl implements CommentService {
     @Autowired
     CommentRepository commentRepository;
-    @Override
-    public Comment save(Comment comment) {
-        return commentRepository.save(comment);
-    }
 
     @Override
     public Set<Comment> findCommentByStatusIdOrderByCreatedDateAsc(Long id) {
         return commentRepository.findCommentByStatusIdOrderByCreatedDateAsc(id);
     }
 
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public List findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Comment findById(Long id) {
+        return commentRepository.findById(id).get();
+    }
+
+    @Override
+    public Comment save(Comment comment) throws SQLIntegrityConstraintViolationException {
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    public Comment update(Comment model) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        Comment comment = findById(id);
+        if( comment != null){
+            commentRepository.delete(comment);
+            return true;
+        }
+        return false;
+    }
 }
