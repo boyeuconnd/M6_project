bc91816e77abf8648b5c9276bd270b33b4b2447a
DungTrinhQ <dungtrinh0702@gmail.com>
2020-08-19 09:21:40 +0700

    thêm query getStatusByAccountId

--- src/main/java/com/nancy/m6project/service/status/StatusService.java
+++ src/main/java/com/nancy/m6project/service/status/StatusService.java
@@ -9,11 +9,15 @@ import java.util.List;
 
 public interface StatusService extends GenericCRUDService<Status> {
     Status save(Status status);
+
     Status findOne(Long id);
+
     List<Status> findStatusByAccount_IdOrderByCreateDateDesc(Long id);
-    List<Status> getAllStatusByAccountId(Long id);
+
+    List<Status> getAllStatusByAccountId(Long id, Integer total_record);
+
     Iterable<Status> findAllByContentContainingAndAccount_Id(String keyword, Long id);
 
-    List<Status> getNewFeed(Long id,Integer total_record);
+    List<Status> getNewFeed(Long id, Integer total_record);
 
 }
